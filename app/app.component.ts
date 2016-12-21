2ce5328489617029539f33446642ccc2152bdc69
Angel Yordanov <AYordanov@lirex.com>
2016-12-21 16:01:13 +0200

    dasdas

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hellooooo {{name}}</h1>`,
+  template: `<h1>Hellooooojoo {{name}}</h1>`,
 })
 export class AppComponent  {
   title = 'Tour of Heroes';
